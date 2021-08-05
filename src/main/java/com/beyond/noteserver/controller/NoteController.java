b5da4b864155f578c1a221ed8f0b50c119f21684
chenshipeng <chenshipeng@ysbang.cn>
2021-08-05 15:03:21 +0800

    change util files path

--- src/main/java/com/beyond/noteserver/controller/NoteController.java
+++ src/main/java/com/beyond/noteserver/controller/NoteController.java
@@ -25,8 +25,23 @@ import java.util.stream.Collectors;
 public class NoteController implements NoteControllerApi {
 
 
-    private static final File CURRENT_REPO_FILE = new File("current_repo");
-    private static final File RUNNING_REPOS_FILE = new File("running_repos");
+    private static final File CURRENT_REPO_FILE;
+    private static final File RUNNING_REPOS_FILE;
+
+
+    static {
+        String filePath = System.getProperty("java.class.path");
+        String pathSplit = System.getProperty("path.separator");//得到当前操作系统的分隔符，windows下是";",linux下是":"
+
+        if(filePath.contains(pathSplit)){
+            filePath = filePath.substring(0,filePath.indexOf(pathSplit));
+        }else if (filePath.endsWith(".jar")) {//截取路径中的jar包名,可执行jar包运行的结果里包含".jar"
+            filePath = filePath.substring(0, filePath.lastIndexOf(File.separator) + 1);
+        }
+
+        CURRENT_REPO_FILE = new File(PathUtils.concat(filePath, "current_repo"));
+        RUNNING_REPOS_FILE = new File(PathUtils.concat(filePath, "running_repos"));
+    }
 
 
     @Override
